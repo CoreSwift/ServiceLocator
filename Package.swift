318815e2493cf7171e535a41fd0c106a4c6303da
Josh Converse <josh.converse@gmail.com>
2022-02-13 12:48:37 -0800

    Set minimum iOS to 13

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "ServiceLocator",
     platforms: [
       .macOS(.v12),
-      .iOS(.v15),
+      .iOS(.v13),
     ],
     products: [
       // Products define the executables and libraries a package produces, and make them visible to other packages.
